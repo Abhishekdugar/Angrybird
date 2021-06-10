de398c190539cc5e06e593cab2af708e2d1ca12b
Abhishekdugar <81962319+Abhishekdugar@users.noreply.github.com>
2021-06-10 14:27:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,168 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+var Ball;
+var Rope;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,600,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    // box1 = new Box(700,320,70,70);
+    // box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    // box3 = new Box(700,240,70,70);
+    // box4 = new Box(920,240,70,70);
+
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    // box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+        box1 = new Box(900, 100, 70, 70); 
+        box2 = new Box(900, 100, 70, 70); 
+        box3 = new Box(900, 100, 70, 70); 
+        box4 = new Box(900, 100, 70, 70); 
+        box5 = new Box(900, 100, 70, 70); 
+        box6 = new Box(900, 100, 70, 70); 
+        box7 = new Box(800, 100, 70, 70); 
+        box8 = new Box(800, 100, 70, 70); 
+        box9 = new Box(800, 100, 70, 70); 
+        box10 = new Box(800, 100, 70, 70);
+        box11 = new Box(800, 100, 70, 70);
+        box12 = new Box(800, 100, 70, 70); 
+        box13 = new Box(700, 100, 70, 70); 
+        box14 = new Box(700, 100, 70, 70); 
+        box15 = new Box(700, 100, 70, 70); 
+        box16 = new Box(700, 100, 70, 70); 
+        box17 = new Box(700, 100, 70, 70); 
+        box18 = new Box(700, 100, 70, 70); 
+        box19 = new Box(700, 100, 70, 70); 
+        box20 = new Box(700, 100, 70, 70); 
+        Ball = new ball(200, 200, 80, 80);
+        Rope = new rope(Ball.body,{x:500,y:50})
+
+
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    
+    pig3.display();
+    pig3.score();
+    log3.display();
+
+    
+    log4.display();
+    log5.display();
+    
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();
+        box1.display(); 
+        box2.display(); 
+        box3.display(); 
+        box4.display() 
+        box5.display() 
+        box6.display() 
+        box7.display() 
+        box8.display() 
+        box9.display() 
+        box10.display()
+        box11.display() 
+        box12.display() 
+        box13.display() 
+        box14.display() 
+        box15.display() 
+        box16.display() 
+        box17.display() 
+        box18.display() 
+        box19.display()
+        box20.display()
+        Ball.display()
+        Rope.display()
+
+
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+       Matter.Body.setPosition(bird.body,{x:200 , y:50});
+       bird.trajectory=[];
+       slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=0600 && hour<=1900){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
